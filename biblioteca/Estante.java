16ea9597c3ecf1ab436e80bdb484dac5b575e06e
andressamenezes <90931741+andressa-freire@users.noreply.github.com>
2021-09-18 09:48:26 -0300

    alteração na classe pessoa

---
+++ biblioteca/Estante.java
@@ -0,0 +1,8 @@
+
+public class Estante {
+    String numero;
+
+    public String getNumero(){
+        return numero;
+    }
+}
--- biblioteca/Pessoa.java
+++ biblioteca/Pessoa.java
@@ -1,4 +1,14 @@
 
 class Pessoa {
+    String nome;
+    String endereço;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEndereço(){
+        return endereço;
+    }
 }
 
