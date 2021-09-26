07cfa10594d89dff4414f2175eb2871071181a07
Raul <raulrufino0@gmail.com>
2021-09-26 15:42:34 -0300

    Adicione esqueleto da classe Item

---
+++ biblioteca/Item.java
@@ -0,0 +1,9 @@
+
+public class Item {
+    // Criando essa classe apenas para conseguir utiliza-la em Biblioteca.java
+    private int ItemID;
+
+    public int getID() {
+        return this.ItemID;
+    }
+}
