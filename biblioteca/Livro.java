1a37d855c97a60dbe839030bf7166c9cd53468dd
andressamenezes <90931741+andressa-freire@users.noreply.github.com>
2021-10-02 16:39:48 -0300

    Mudanças classe Livro
    Adicionei o restante dos atributos , getters e setters

--- biblioteca/Livro.java
+++ biblioteca/Livro.java
@@ -1,6 +1,11 @@
 
 public class Livro {
     private String ISBN;
+    private int publicacao;
+    private String titulo;
+    private int paginas;
+    private int numEstante;
+    private String[] categoria = new String[100];
 
     public String getISBN() {
         return ISBN;
@@ -9,4 +14,45 @@ public class Livro {
     public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
+
+    public int getPublicacao() {
+        return publicacao;
+    }
+
+    public void setPublicacao(int publicacao) {
+        this.publicacao = publicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public int getNumEstante() {
+        return numEstante;
+    }
+
+    public void setNumEstante(int numEstante) {
+        this.numEstante = numEstante;
+    }
+
+    public String[] getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String[] categoria) {
+        this.categoria = categoria;
+    }
+
 }
